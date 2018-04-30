3e8e80beae76344e2cbac6f9abc54c02ae113be5
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-04-29 21:19:24 -0500

    Create p40.java

---
+++ p40.java
@@ -0,0 +1,31 @@
+ //40
+ // O(n) time, O(n) space. hashmap.
+ // O(n) time, O(n) space. count array.
+
+public class p40 {
+
+  public static int findDuplicateUsingHashMap(int[] nums) {
+    HashMap <Integer, Integer> hashmap = new HashMap <> ();
+    for (int i = 0; i < nums.length; i++) {
+      if (hashmap.containsKey(nums[i])) {
+        return nums[i];
+      } else {
+        hashmap.put(nums[i], 0);
+      }
+    }
+    return -1;
+ }
+ 
+  public static int findDuplicateUsingCountArray(int[] nums) {
+    int[] mem = new int[nums.length];
+    for (int i = 0; i < nums.length; i++) {
+      if (mem[nums[i]] > 0) {
+        return nums[i];
+      } else {
+        mem[nums[i]] += 1;
+      }
+    }
+    return -1;
+  }
+  
+}
