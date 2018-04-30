217905fc5361d4dc89247ffe62a19197d68f700b
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-04-29 19:02:34 -0500

    Create p12.java

---
+++ p12.java
@@ -0,0 +1,26 @@
+//12
+//binary search. O(logn) time O(1) space.
+public class p12 {
+
+  public static int binarySearch(int target, int[] input) {
+  
+    int floor = -1;
+    int ceiling = input.length;
+
+    while (ceiling - floor > 1) {
+      guessIndex = floor + (ceiling - floor)/2;
+      guessValue = input[guessIndex];
+      
+      if(guessValue == target) {
+        return guessIndex
+      }
+      if(guessValue > target) {
+        ceiling = guessIndex;
+      }else {
+        floor = guessIndex;
+      }
+      return -1;
+    }
+  }
+  
+}
