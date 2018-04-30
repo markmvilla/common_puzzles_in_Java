fd89ba4a3ce0d7602c78fa303fa8b0a5bc2e106f
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-04-29 21:58:27 -0500

    Update p15.java

--- p15.java
+++ p15.java
@@ -2,7 +2,8 @@
 // 4/20/18
 // Fibonacci
 // O(2^n) time, O(n) space. recursively. O(2^n) time, O(n) space.
-// O(n) time, O(1) space. using memoization iteratively.
+// O(n) time, O(1) space. dynamicly iterativly (bottom-up).
+// O(lgn) time, O(lgn) space. dynamicaly iterate matrix.
 
 class p15 {
 
@@ -10,22 +11,16 @@ class p15 {
     if (n<=0) return n;
     return fib(n-1) + fib(n-2);
   }
-  
-  public static int fibonacciIteratively(int n) {
-    if(n <= 1) {
-			return n;
+	
+	public static int fibonacciIteratively(int n) {
+		int a = 0, b = 1, temp;
+		if (n == 0) return a;
+		for (int i=2; i<=n; i++) {
+			temp = a+b;
+			a = b;
+			b = temp;
 		}
-    
-    int current = 0;
-    int firstPrevious = 1;
-    int secondPrevious = 0;
-    
-    for (int i = 2; i < n; i++) {
-      current = firstPrevious + secondPrevious;
-      firstPrevious = current;
-      secondPrevious = firstPrevious;
-    }
-    return current;
-  }
-  
+		return b;
+	}	
+	
 }
