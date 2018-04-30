1d1b6b8ac9215681b4ba96d03dd5dd199e2a451d
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-04-29 19:34:41 -0500

    Update p2.java

--- p2.java
+++ p2.java
@@ -1,6 +1,7 @@
-//Mark Villa
-//4/20/18
-//O(n) time O(n) space. maintain a left[] and right[] product for each i. return out[] with each left[] * right[].
+// Mark Villa
+// 4/20/18
+// Array of Products
+// O(n) time O(n) space. maintain a left[] and right[] product for each i. return out[] with each left[] * right[].
 
 class Product {
    public int[] products(int[] values) {
