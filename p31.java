acf4cb111e5004a87f696874bb7fc9e22e3acfcb
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-05 16:06:58 -0500

    Update p31.java

--- p31.java
+++ p31.java
@@ -3,18 +3,18 @@
 //  Recursive String Permutation
 //  O(n^2 * n!) time, undetermined space. base: length = 0. recursive: for each letter (truncated original, updated perm).
 
-class p31 {
+public class p31 {
   
-  void permutation(String str) {
-    permutation(str, "");
-  }
+	public static void main(String[] args) {
+		permutation("test", "");
+	}
   
 	public static void permutation(String original, String perm) {
 	    if (original.length() == 0)
 	      System.out.println(perm);
 	   else {
 	      for (int i = 0; i < original.length(); i++) 
-	        permutation(original.substring(0,i) + original.substring(i + 1), perm + original.charAt(i));
+					permutation(original.substring(0,i) + original.substring(i + 1), perm + original.charAt(i));
 	    }   
 	}
   
