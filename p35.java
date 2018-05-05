db4acaf216b9965130f5d6578e61f4ccb8157dcb
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-05 17:33:23 -0500

    Create p35.java

---
+++ p35.java
@@ -0,0 +1,16 @@
+public p35 {
+
+  public String[] shuffle(String[] data ) {
+
+        String temp ;
+        for (int i=0; i< data.length ; i++) {
+            int dest = (int) Math.floor((Math.random() * data.length));
+            temp = data[i] ;
+            data[i] = data[dest] ;
+            data[dest] = temp ;
+        }
+
+        return data ;
+    }
+
+}
