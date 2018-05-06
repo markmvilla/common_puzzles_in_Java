02fcd4be7bfa4d98c48f2227a3e1de68aa5f8b0f
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-06 01:12:01 -0500

    Update p14.java

--- p14.java
+++ p14.java
@@ -1,2 +1,19 @@
+//  Mark Villa
+//  4/20/18
+//  In Flight Entertainment System
+//  O(n) time, O(n) space. 
 
+import java.util.*;
 
+public class P14 {
+
+    public Boolean isAvailable(Integer flightLength , Integer[] movies) {
+        Map<Integer, Integer> sums = new HashMap<Integer, Integer>();
+        for (Integer movieLength : movies) {
+            if (sums.get(movieLength) != null) return true ;
+            sums.put(flightLength - movieLength, 1) ;
+        }
+        return false;
+    }
+
+}
