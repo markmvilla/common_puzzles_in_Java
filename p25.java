d7fd26ae0c96e22a6be7a16097bd73dad9b887a0
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-06 01:30:24 -0500

    Create p25.java

---
+++ p25.java
@@ -0,0 +1,11 @@
+//  Mark Villa
+//  4/20/18
+//  Kth Node
+//  O(n) time, O(1) space. if size given. call next k times.
+//  O(n) time, O(1) space. if doubly linked. call prev k times from tail.
+//  O(n) time, O(1) space. if neither, have a delayed pointer by k nodes.
+
+public class P25 {
+    public LinkedListNode kthToLastNode() {
+    }
+}
