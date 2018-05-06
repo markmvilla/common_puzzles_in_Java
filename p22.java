816afd01c7dec39a1bf0b09c54a88f56bb11e668
markmvilla <34073689+markmvilla@users.noreply.github.com>
2018-05-06 01:20:55 -0500

    Create p22.java

---
+++ p22.java
@@ -0,0 +1,12 @@
+//  Mark Villa
+//  4/20/18
+//  Delete Node
+//  O(1) time, O(1) space. copy next node to current node. if no ref. then keep prev and current pointer and do prev.next = current.next when at node to be deleted.
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class P22 {
+    public void deleteNode() {
+    }
+}
